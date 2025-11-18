31cf3128281772ef464ce05021d2ca0cee66e9af
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-11-18 11:04:01 +0500

    Update abauihub.lua

--- raw/abauihub.lua
+++ raw/abauihub.lua
@@ -1,8 +1,8 @@
 local support = {
-    [3101667897] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/LOS.lua",
-    [121864768012064] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/fishit.lua",
-    [16812449989] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/fpe.lua",
-    [135880624242201] = "https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/CT.lua"
+    [3101667897] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/LOS.lua",
+    [121864768012064] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fishit.lua",
+    [16812449989] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fpe.lua",
+    [135880624242201] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/CT.lua"
 }
 
 local place = game.PlaceId
@@ -11,5 +11,5 @@ local url = support[place]
 if url then
     loadstring(game:HttpGet(url))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CaseohCASEOH/aabbaaii/refs/heads/main/2023479888634011.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/2023479888634011.lua"))()
 end
