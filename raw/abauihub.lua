851362defed478c55a118c0d0f7bc4dbbf0978ad
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-11-23 03:43:45 +0500

    Add loader.lua URL to support table

--- raw/abauihub.lua
+++ raw/abauihub.lua
@@ -3,6 +3,7 @@ local support = {
     [121864768012064] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fishit.lua",
     [16812449989] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fpe.lua",
     [135880624242201] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/CT.lua"
+    [155615604] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/loader.lua"
 }
 
 local place = game.PlaceId
