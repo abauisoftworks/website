cb700108c6b0bdca7b9b47e047e7d22af1732b99
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-11-23 01:53:06 +0500

    Create plsa.lua

---
+++ raw/plsa.lua
@@ -0,0 +1,96 @@
+-- waits the game to load before execution.
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+-- made by zins in scriptblox, i added this bypasser if aesthetical added adonis.
+local g = getinfo or debug.getinfo
+local d = false
+local h = {}
+
+local x, y
+
+setthreadidentity(2)
+
+for i, v in getgc(true) do
+    if typeof(v) == "table" then
+        local a = rawget(v, "Detected")
+        local b = rawget(v, "Kill")
+    
+        if typeof(a) == "function" and not x then
+            x = a
+            
+            local o; o = hookfunction(x, function(c, f, n)
+                if c ~= "_" then
+                    if d then
+                        warn(`Adonis AntiCheat flagged\nMethod: {c}\nInfo: {f}`)
+                    end
+                end
+                
+                return true
+            end)
+
+            table.insert(h, x)
+        end
+
+        if rawget(v, "Variables") and rawget(v, "Process") and typeof(b) == "function" and not y then
+            y = b
+            local o; o = hookfunction(y, function(f)
+                if d then
+                    warn(`Adonis AntiCheat tried to kill (fallback): {f}`)
+                end
+            end)
+
+            table.insert(h, y)
+        end
+    end
+end
+
+local o; o = hookfunction(getrenv().debug.info, newcclosure(function(...)
+    local a, f = ...
+
+    if x and a == x then
+        if d then
+            warn(`zins | adonis bypassed`)
+        end
+
+        return coroutine.yield(coroutine.running())
+    end
+    
+    return o(...)
+end))
+
+setthreadidentity(7)
+
+-- important notifications.
+StarterGui:SetCore("SendNotification", {
+  Title = "! IMPORTANT WARNING !",
+  Text = "This Script Is Pre-Alpha, May Contain Lot Of Bugs.",
+  Duration = 15,
+  Callback = nil,
+  Button1 = "Understood."
+})
+StarterGui:SetCore("SendNotification", {
+  Title = "? IMPORTANT NOTE ?",
+  Text = "This Script Has No GUI.",
+  Duration = 12.5,
+  Callback = nil,
+  Button1 = "Okay."
+})
+StarterGui:SetCore("SendNotification", {
+  Title = "✓ HOW TO TOGGLE ✓",
+  Text = "Rejoin Server To Disable The Script.",
+  Duration = 7.5,
+  Callback = nil,
+  Button1 = "Understood."
+})
+StarterGui:SetCore("SendNotification", {
+  Title = "? HOW TO USE ?",
+  Text = "Make Sure Enable Shiftlock, Then Enemy Should Be Visible On Screen.",
+  Duration = 10,
+  Callback = nil,
+  Button1 = "Understood."
+})
+
+-- plsa executer.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/PLSA.lua"))()
