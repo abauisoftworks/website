54bdd291cc56ebb8b7c7bfb06fd2fef8538f2356
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2025-12-27 17:28:08 +0500

    Fix case sensitivity in loader script URL

--- raw/wildhub.lua
+++ raw/wildhub.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/noob-scripts/some-scripts/refs/heads/master/Wild%20Hub/loader.lua"))()
+-- https://discord.gg/mhzDcapGgA
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noob-scripts/some-scripts/refs/heads/master/Wild%20Hub/Loader.lua"))()
