9ce3d07b4430c0be399ee68a11f1c2f3abe49049
aabbaaii <172551149+aabbaaii13@users.noreply.github.com>
2026-01-01 13:46:52 +0500

    Update abauihub.lua

--- raw/abauihub.lua
+++ raw/abauihub.lua
@@ -2,7 +2,7 @@ local support = {
     [3101667897] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/LOS.lua",
     [121864768012064] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fishit.lua",
     [16812449989] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/fpe.lua",
-    [135880624242201] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/CT.lua"
+    [135880624242201] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/CT.lua",
     [155615604] = "https://raw.githubusercontent.com/aabbaaii13/aabbaaii/refs/heads/main/loader.lua"
 }
 
